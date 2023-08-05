5c1018fbaeb0a496529771117bc71043db1367bd
邢浩东 <5487156+kuaishou1@user.noreply.gitee.com>
2023-08-05 22:37:27 +0800

    feat: 添加icon组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
 import SubMenu from './components/Menu/subMenu';
-
-
+import Icon from './components/Icon/icon';
+import Transition from './components/Transition';
+library.add(fas);
 function App() {
+  const [show, setShow] = React.useState(true);
   return (
     <div className="App">
       <header className="App-header">
+        <Button size={ButtonSize.Large} onClick={() => { setShow(!show) }}>toggle</Button>
+        <Transition
+          in={show}
+          timeout={300}
+          animation='zoom-in-bottom'
+        >
+          <div>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+          </div>
+        </Transition>
+        <Transition
+
+          in={show}
+          timeout={300}
+          animation='zoom-in-left'
+          wrapper
+        >
+          <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>A Large Button</Button>
+        </Transition>
+
+        <Icon icon='arrow-down' theme='primary' size='10x' />
         <FontAwesomeIcon icon={faCoffee} size='10x' />
         <Menu defaultIndex='0' onSelect={(inde) => { console.log(inde) }} >
           <MenuItem>
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,80 +1,83 @@
-import classNames from "classnames";
-import React, { useContext, useState } from "react";
-import { MenuContext } from "./menu";
-import { MenuItemProps } from "./menuItem";
-import { title } from "process";
-
+import React, { FC, useContext, useState, FunctionComponentElement, ReactNode } from 'react'
+import classNames from 'classnames'
+import { MenuContext } from './menu'
+import { MenuItemProps } from './menuItem'
+import Icon from '../Icon/icon'
+import Transition from '../Transition/transition'
 export interface SubMenuProps {
-    title: string;
-    disabled?: boolean;
-    className?: string;
-    style?: React.CSSProperties;
     index?: string;
-    children?: React.ReactNode;
+    /**下拉菜单选项的文字 */
+    title: string;
+    /**下拉菜单选型的扩展类名 */
+    className?: string;
+    children?: ReactNode;
 }
-const SubMenu: React.FC<SubMenuProps> = (props) => {
-    const { index, className, disabled, title, children } = props;
 
-    const context = useContext(MenuContext);
-    const openedSubMenus = context.defaultOpenSubMenus as Array<string>;
-    const isOpened = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false;
-    const [menuOpen, setOpen] = useState(isOpened);
+export const SubMenu: FC<SubMenuProps> = ({ index, title, children, className }) => {
+    const context = useContext(MenuContext)
+    const openedSubMenus = context.defaultOpenSubMenus as Array<string>
+    const isOpend = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false
+    const [menuOpen, setOpen] = useState(isOpend)
     const classes = classNames('menu-item submenu-item', className, {
-        "is-disabled": disabled,
-        "is-active": context.index === index,
-    });
+        'is-active': context.index === index,
+        'is-opened': menuOpen,
+        'is-vertical': context.mode === 'vertical'
+    })
     const handleClick = (e: React.MouseEvent) => {
-        e.preventDefault();
-        setOpen(!menuOpen);
+        e.preventDefault()
+        setOpen(!menuOpen)
     }
-    let timer: any;
+    let timer: any
     const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
-        e.preventDefault();
+        clearTimeout(timer)
+        e.preventDefault()
         timer = setTimeout(() => {
-            setOpen(toggle);
-        }, 300);
+            setOpen(toggle)
+        }, 300)
     }
     const clickEvents = context.mode === 'vertical' ? {
         onClick: handleClick
-    } : {};
+    } : {}
     const hoverEvents = context.mode !== 'vertical' ? {
         onMouseEnter: (e: React.MouseEvent) => { handleMouse(e, true) },
-        onMouseLeave: (e: React.MouseEvent) => { handleMouse(e, false) },
-    } : {};
+        onMouseLeave: (e: React.MouseEvent) => { handleMouse(e, false) }
+    } : {}
     const renderChildren = () => {
         const subMenuClasses = classNames('viking-submenu', {
-            'menu-opened': menuOpen,
+            'menu-opened': menuOpen
         })
         const childrenComponent = React.Children.map(children, (child, i) => {
-            const childElement = child as React.FunctionComponentElement<MenuItemProps>;
-            const { displayName } = childElement.type;
-            if (displayName === 'MenuItem') {
-                return React.cloneElement(childElement, { index: `${index}-${i}` });
+            const childElement = child as FunctionComponentElement<MenuItemProps>
+            if (childElement.type.displayName === 'MenuItem') {
+                return React.cloneElement(childElement, {
+                    index: `${index}-${i}`
+                })
             } else {
-
-                console.error('Warning: SubMenu has a child which is not a MenuItem component');
+                console.error("Warning: SubMenu has a child which is not a MenuItem component")
             }
         })
         return (
-            <ul className={subMenuClasses}>
-                {childrenComponent}
-            </ul>
+            <Transition
+                in={menuOpen}
+                timeout={300}
+                animation="zoom-in-top"
+            >
+                <ul className={subMenuClasses}>
+                    {childrenComponent}
+                </ul>
+            </Transition>
         )
     }
-
     return (
         <li key={index} className={classes} {...hoverEvents}>
-            <div className="submenu-title" onClick={handleClick} {...clickEvents}>
+            <div className="submenu-title" {...clickEvents}>
                 {title}
+                <Icon icon="angle-down" className="arrow-icon" />
             </div>
             {renderChildren()}
         </li>
     )
-
 }
 
-SubMenu.displayName = 'SubMenu';
+SubMenu.displayName = 'SubMenu'
 export default SubMenu;
-
-
-
