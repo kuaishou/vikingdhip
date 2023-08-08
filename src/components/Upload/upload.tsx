c61371baa4ab735c448224421e58b6808aad5b4d
邢浩东 <5487156+kuaishou1@user.noreply.gitee.com>
2023-08-08 23:45:56 +0800

    ff

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useRef, useState } from 'react'
 import axios from "axios"
+import UploadList from './uploadList'
 import { Button } from '../Button/button';
 
 
@@ -18,24 +19,29 @@ export interface UploadFile {
 
 export interface uploadProps {
     action: string;
+    defaultFileList?: UploadFile[];
     beforeUpload?: (file: File) => boolean | Promise<File>;
     onProgress?: (percentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
     onChange?: (file: File) => void;
+    onRemove?: (file: UploadFile) => void;
+    
 }
 
 export const Upload: FC<uploadProps> = (props) => {
     const {
         action,
+        defaultFileList,
         beforeUpload,
         onProgress,
         onSuccess,
         onError,
-        onChange
+        onChange,
+        onRemove
     } = props
     const fileInput = useRef<HTMLInputElement>(null)
-    const [fileList, setFileList] = useState<UploadFile[]>([])
+    const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList||[])
     // const [uploading, setUploading] = useState(false)
     const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
         setFileList(prevList => {
@@ -109,6 +115,14 @@ export const Upload: FC<uploadProps> = (props) => {
             }
         })
     }
+    const handleRemove = (file: UploadFile) => {
+        setFileList(prevList => {
+            return prevList.filter(item => item.uid !== file.uid)
+        })
+        if (onRemove) {
+            onRemove(file)
+        }
+    }
     const uploadFiles = (files: FileList) => {
         let postFiles = Array.from(files)
         postFiles.forEach(file => {
@@ -132,6 +146,7 @@ export const Upload: FC<uploadProps> = (props) => {
         <div className='viking-upload-component'>
             <Button btnType='primary' onClick={handleClick}>上传文件</Button>
             <input ref={fileInput} type="file" className='viking-file-input' style={{ display: 'none' }} onChange={handleFileChange} />
+            <UploadList fileList={fileList} onRemove={handleRemove}/>
         </div>
     )
 }
